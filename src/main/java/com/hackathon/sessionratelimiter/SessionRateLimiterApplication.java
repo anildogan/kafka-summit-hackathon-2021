cd741c6c610deba72885edcf945ff0e7b32af8a8
Anil Dogan <anil.dogan@trendyol.com>
2021-05-06 16:55:10 +0300

    initial commit

---
+++ src/main/java/com/hackathon/sessionratelimiter/SessionRateLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.sessionratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionRateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionRateLimiterApplication.class, args);
+	}
+
+}
